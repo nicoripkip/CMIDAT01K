4c978d231aa4a562adbb6ae8970a977a770f5faf
nicoripkip <nicovanommen.nvo@gmail.com>
2022-04-12 22:29:06 +0200

    Eerste werkende prototype

--- src/main.cpp
+++ src/main.cpp
@@ -13,28 +13,35 @@
 #include "headers/communication.hpp"
 
 
-#define 	ENABLE_DISPLAY 		true
-#define 	ENABLE_LORA 		true
-#define 	ENABLE_SERIAL 		true
-#define 	PABOOST 			true
+#define 	ENABLE_DISPLAY 						true
+#define 	ENABLE_LORA 						true
+#define 	ENABLE_SERIAL 						true
+#define 	PABOOST 							true
 
-#define 	BAUD_RATE 			115200
-#define 	DEVICE_ADDRESS		5
-#define		SCL_ADDRESS			0xC
-#define 	SDA_ADDRESS			0x0D
+#define 	BAUD_RATE 							115200
+#define 	DEVICE_ADDRESS						0x05
+#define		SCL_ADDRESS							0x0C
+#define 	SDA_ADDRESS							0x0D
 
-#define TC74_TEMPERATURE_SENSOR_ADDRESS 0x4A
-#define RELAY_PIN						0x20
+#define 	TC74_TEMPERATURE_SENSOR_ADDRESS 	0x4A
+#define 	RELAY_PIN							0x20
+#define 	LCD_SCREEN_ADDRESS 					0x27
 
 
 
 const uint32_t license[4] = { 0xA13202B6,0x8B58DCC0,0x4E7F3DFD,0x6F51BFA3 };
 const char devideID[13] = "dc2014f7c630";
-Communication wifi("api.thingspeak.com", 80);
-int data;
+
+bool state = false;
+bool take_over_state = false;
+int temperature_detected[10];
+int temp_counter;
+int toggle_flag = 0;
 
 
 int TC74_TEMPERATURE_get();
+void switch_fan_state();
+int get_temp_average();
 
 
 /**
@@ -62,12 +69,52 @@ void loop()
 {
 	Blynk.run();
 
-	digitalWrite(RELAY_PIN, HIGH);
-	delay(500);
-	digitalWrite(RELAY_PIN, LOW);
-	delay(500);
-
 	int temp = TC74_TEMPERATURE_get();
+	temperature_detected[temp_counter] = temp;
+  	temp_counter++;
+
+	if (temp_counter > 9) 
+	{
+		temp_counter = 0;
+	}
+
+	int temp_comp = get_temp_average();
+	if (temp_comp > 26) 
+	{
+		toggle_flag = 1;
+	} else if (temp_comp <= 25) 
+	{
+		toggle_flag = 0;
+	}
+
+	if (take_over_state == 1) 
+	{
+		switch_fan_state();
+	}
+	else 
+	{
+		digitalWrite(RELAY_PIN, state);
+	}
+}
+
+
+/**
+ * @brief Construct a new blynk write object
+ * 
+ */
+BLYNK_WRITE(V0)
+{
+	state = !state;
+}
+
+
+/**
+ * @brief Construct a new blynk write object
+ * 
+ */
+BLYNK_WRITE(V2)
+{
+	take_over_state = !take_over_state;
 }
 
 
@@ -91,4 +138,38 @@ int TC74_TEMPERATURE_get()
   Serial.println(temperature);
 
   return temperature;
+}
+
+
+/**
+ * @brief Functie voor het toggelen van de ventilator
+ * 
+ */
+void switch_fan_state()
+{
+  switch (toggle_flag) 
+  {
+    case 0: digitalWrite(RELAY_PIN, LOW); break;
+    case 1: digitalWrite(RELAY_PIN, HIGH); break;
+  }
+
+  Serial.print("[info]\t\tStaat van de fan: ");
+  Serial.println(toggle_flag);
+}
+
+
+/**
+ * @brief Functie voor het verkrijgen van het gemiddelde van de temperatuur
+ * 
+ */
+int get_temp_average()
+{
+  int i, t = 0;
+
+  for (i = 0; i < 10; i++)
+  {
+    t += temperature_detected[i];
+  }
+
+  return t / 10;
 }
